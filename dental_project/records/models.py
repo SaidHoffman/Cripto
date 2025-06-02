152549e62374a75c4ea4acbe722d40c6386786d4
SaidHoffman <ssigalam1800@alumno.ipn.mx>
2025-06-02 13:49:56 -0600

    CifradoAESCBC

---
+++ dental_project/records/models.py
@@ -0,0 +1,13 @@
+# records/models.py
+from django.db import models
+
+class EncryptedRecord(models.Model):
+    patient_name = models.CharField(max_length=150, unique=True)  # antes: patient_id
+    ciphertext     = models.BinaryField()
+    iv             = models.BinaryField()
+    wrap_key       = models.BinaryField()
+    signature      = models.BinaryField()
+    timestamp      = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.patient_name
